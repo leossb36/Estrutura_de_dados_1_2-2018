936ed82abdef3fdf7a26d5cdd3c9a98f51f44f9f
Leonardo dos Santos <leossb36@gmail.com>
2018-08-31 11:37:08 -0300

    First project
    the first question its done

---
+++ projeto 1/src/main1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#define MAX 10
+
+
+void print_vector(double *vector, double elements);
+void fneuronio(double *ENTRADAS, double *PESOS, double limiar, double len, int *status);
+
+int main ()
+{
+    double ENTRADAS[MAX] = {0};
+    double PESOS[MAX] = {0};
+    double T;
+    int status = 0;
+
+    printf("Enter with 10 value of ENTRADAS:\n");
+
+    for (int i = 0; i <= MAX; i++)
+    
+        scanf("%lf", &ENTRADAS[i]);
+    
+    print_vector(ENTRADAS, MAX);
+
+    printf("Enter with 10 value of PESOS:\n");
+
+    for (int j = 0; j <= 10; j++)
+
+        scanf("%lf", &PESOS[j]);
+
+    print_vector(PESOS, MAX);
+
+    printf("Enter with the value of limiar:\n");
+
+    scanf("%lf", &T);
+
+    printf("\n\nLimiar = %.4lf\n", T);
+
+    fneuronio(ENTRADAS, PESOS, T, 10, &status);
+
+    if (status == 1)
+        printf("\nNeurônio ativado!\n");
+
+    else     
+        printf("\nNeurôno inibido!\n");
+
+    return 0;
+}
+
+void print_vector(double *vector, double elements)
+{
+    printf("Valores:\n");
+
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%.4lf\n", *(vector + i));
+    }
+}
+
+void fneuronio(double *ENTRADAS, double *PESOS, double limiar, double len, int *status)
+{
+    double SOMAP = 0;
+
+    for (int i = 0; i < len; i++)
+
+        SOMAP += ENTRADAS[i] * PESOS[i];
+
+    *status = SOMAP > limiar;
+
+}
+
+
+
