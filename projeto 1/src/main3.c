ba48946145e767c7a47750604362390b03cbd57b
Leonardo dos Santos <leossb36@gmail.com>
2018-09-03 20:52:24 -0300

    Adding count moviments

--- projeto 1/src/main3.c
+++ projeto 1/src/main3.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 void mov_disk(int disk, char orig, char aux, char dest);
+void print_mov(int disk);
 
 int main()
 {
     int disk;
 
     printf("\nEnter with disc amount:\n");
+    
     scanf("%d", &disk);
-
+    
+    print_mov(disk);
+    
     mov_disk(disk, 'A', 'B', 'C');
-
+    
     return 0;
 }
 
@@ -27,5 +32,16 @@ void mov_disk(int disk, char orig, char aux, char dest)
     printf("Move disc %d from %c to %c\n", disk, orig, dest);
 
     mov_disk(disk-1, aux, orig, dest);
+}
 
+void print_mov(int disk)
+{
+    int movAmount = 1;
+
+    for (int i = 0; i < disk; i++)
+        movAmount *= 2;
+
+    movAmount -= 1;    
+
+    printf("\nMoviment Amount = %d\n\n", movAmount);
 }
