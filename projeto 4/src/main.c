b85b21e4cb8a861bdbc5675d7ce8993d7b0c5cc2
Bruno-Felix <bruno-a.f@hotmail.com>
2018-10-31 09:33:05 -0300

    Updating Aleatory Number of Flights Function

--- projeto 4/src/main.c
+++ projeto 4/src/main.c
@@ -2,9 +2,25 @@
 
 int main(){
 	int landings, take_off;
+    int *p_take_off, *p_landings;
     
     landings = randomFly(1);
     take_off = randomFly(2);
+
+    p_landings = (int*)(malloc(sizeof(int)));
+    if(p_landings == NULL){
+        printf("\nError: Fail to allocate memory - p_landings!\n");
+        exit(0);
+    }
+    
+	p_take_off = (int*)(malloc(sizeof(int)));
+    if(p_take_off == NULL){
+        printf("\nError: Fail to allocate memory - p_take_off!\n");
+        exit(0);
+    }
+    
+	*p_landings = landings;
+	*p_take_off = take_off;
     
     int fly_number = landings + take_off;
 
@@ -20,9 +36,9 @@ int main(){
     printf("%d\n", fly_number);
 
     printf("Número de Aproximações: ");
-    printf("%d\n", landings);
+    printf("%d\n", *p_landings);
 
     printf("Número de Decolagens: ");
-    printf("%d\n", take_off);
+    printf("%d\n", *p_take_off);
 return 0;
 }
